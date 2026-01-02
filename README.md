1cd33ee0e0c50663aaa0e6d0af2c776049fe9a3d
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2026-01-02 14:13:09 +0100

    Initialise project with uv

---
+++ README.md
@@ -0,0 +1,3 @@
+# marsh-sim
+
+Python utilities for [MARSH-Sim])(https://marsh-sim.github.io/).
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from python-marsh-sim!")
+
+
+if __name__ == "__main__":
+    main()
